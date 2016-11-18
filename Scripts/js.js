294c7bde28841af952cdef0da9c30088c85beeb8
anonymous <anonymous>
2016-11-18 15:20:08 -0800

    url path fixes

---
+++ Scripts/js.js
@@ -0,0 +1,1 @@
+"use strict";(function(){var n=function(n,t){for(var r=document.querySelectorAll(n),i=0;i<r.length;i++)t(r[i])},t=function(){var t=this.getAttribute("data-target"),i=this.getAttribute("data-toggle");n(t,function(n){n.classList.toggle(i)})},i=function(){n(".navbar-toggle",function(n){n.addEventListener("click",t)})};i()})();
